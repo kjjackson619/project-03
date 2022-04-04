47489e4dccff10cbb25e39902e1163b940642854
ChastainJon <chastain.r.jon@gmail.com>
2022-04-04 19:38:46 -0400

    added shirt schema

---
+++ server/models/Shirt.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const shirtSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    color: {
+        type: String,
+    },
+    image: {
+        type: String
+    },
+    price:{ 
+        type: Number,
+        required: true,
+        min: 4.00
+    },
+    category:{
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    }
+})
+
+const Shirt = mongoose.model('Shirt', shirtSchema)
+
+module.exports = Shirt
