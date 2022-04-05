062050ca7a21ece6b7e206c78394e271649dea48
KJ Jackson <kjjackson619@yahoo.com>
2022-04-04 20:39:42 -0400

    form state

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,38 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 function Account() {
 
+    const [formState, setFormState] = useState({ name: '', email: '', password: '' });
+
+    const { name, email, password } = formState;
+
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    }
+
+    console.log(formState);
+
     return (
         <section>
             <div>
-                <h1>Login or Sign Up</h1>
+                <h2>Login</h2>
                 <form id="login-form">
                     <div>
                         <label htmlFor="email">Email:</label>
-                        <input type="email" name="email" />
+                        <input type="email" defaultValue={email} onChange={handleChange} name="email" />
                     </div>
                     <div>
                         <label htmlFor="password">Password:</label>
-                        <input type="text" name="password" />
+                        <input type="text" defaultValue={password} onChange={handleChange} name="password" />
                     </div>
                     <button type="submit">Submit</button>
                 </form>
             </div>
             <br />
             <div>
+                <h2>Sign Up</h2>
                 <form id="signup-form">
                     <div>
                         <label htmlFor="name">Name:</label>
-                        <input type="text" name="name"></input>
+                        <input type="text" defaultValue={name} name="name" onChange={handleChange} ></input>
                     </div>
                     <div>
                         <label htmlFor="email">Email:</label>
-                        <input type="email" name="email-address" />
+                        <input type="email" defaultValue={email} onChange={handleChange} name="email-address" />
                     </div>
                     <div>
                         <label htmlFor="password">Password:</label>
-                        <input type="text" name="password-create" />
+                        <input type="text" defaultValue={password} onChange={handleChange} name="password-create" />
                     </div>
                     <button type="submit">Submit</button>
                 </form>
