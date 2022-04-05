de8aa500dd856614fe02352d5c4ac35322ccd148
KJ Jackson <kjjackson619@yahoo.com>
2022-04-04 20:02:53 -0400

    nav categories

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+function Nav() {
+
+    return (
+        <header>
+            <h1>
+                <a href="/">
+                    <span></span>
+                    Ecommerce
+                </a>
+            </h1>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-1">
+                        <a href="#account">
+                            <p>
+                                Account
+                            </p>
+                        </a>
+                    </li>
+                    <li className="mx-1">
+
+                        <a href="#cart">
+                            <p>
+                                🛒 View Cart
+                            </p>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Nav;
