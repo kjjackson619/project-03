a0f55b45215b8951ee2259bbc22b42f046029293
ChastainJon <chastain.r.jon@gmail.com>
2022-04-11 19:46:14 -0400

    added queries to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express')
 const { User, Shirt, Category, Order } = require('../models')
 const { signToken } = require('../utils/auth')
+const stripe = require('stripe')('sk_test_51KnWTLJspjbSsWAyeelYzgt21wbCIe7TLNZXztNJajiYsFcJUZ7yBA75WzYJfEVMK5QqgvkgAdAG3NoiEDcDGBFB00s2PZ3R5M')
+
