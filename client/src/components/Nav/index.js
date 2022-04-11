4f3a99a1beafd607c1c33b002525925c88b19ad2
KJ Jackson <kjjackson619@yahoo.com>
2022-04-11 19:37:38 -0400

    nav functionality

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,44 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+import SignUp from '../Signup';
+import Login from '../Login';
+
+import Auth from '../../utils/auth';
 
 
-function Nav() {
+const Nav = () => {
+    const [showModal, setShowModal] = useState(false);
 
     return (
-        <header>
-            <h1>
-                <a href="/">
-                    <span></span>
-                    Ecommerce
-                </a>
-            </h1>
-            <nav>
-                <ul className="flex-row">
-                    <li className="mx-2">
-                        <a href="#login">
-                            <p>
-                                Login
-                            </p>
-                        </a>
-                    </li>
-                    <li className="mx-2">
-                        <a href="#signup">
-                            <p>
-                                SignUp
-                            </p>
-                        </a>
-                    </li>
-                    <li className="mx-1">
-
-                        <a href="#cart">
-                            <p>
-                                🛒 View Cart
-                            </p>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <Navbar>
+                <Container fluid>
+                    <Navbar.Brand as={Link} to='/'>
+                        Ecommerce Shop
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls='navbar' />
+                    <Navbar.Collapse id='navbar'>
+                        <Nav className='ml-auto'>
+                            {Auth.loggedIn() ? (
+                                <>
+                                    <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                                </>
+                            ) : (
+                                <Nav.Link onClick={() => setShowModal(true)}>Login/SignUp</Nav.Link>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Modal
+                size='lg'
+                show={showModal}
+                onHide={() => setShowModal(false)}
+                aria-labelledby='signup-modal'>
+                <Tab.Container defaultActiveKey='login'>
+                    <Modal.Header closeButton>
+                        <Modal.Title id='signup-modal'>
+                            <Nav variant='pills'>
+                                <Nav.Item>
+                                    <Nav.Link eventKey='login'>Login</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Tab.Content>
+                            <Tab.Pane eventKey='login'>
+                                <Login handleModalClose={() => setShowModal(false)} />
+                            </Tab.Pane>
+                            <Tab.Pane eventKey='signup'>
+                                <SignUp handleModalClose={() => setShowModal(false)} />
+                            </Tab.Pane>
+                        </Tab.Content>
+                    </Modal.Body>
+                </Tab.Container>
+            </Modal>
+        </>
     );
-}
+
+};
+
 
 export default Nav;
