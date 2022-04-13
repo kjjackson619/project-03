1e085164f9ecad4bc10b95a910c4528d182bdd04
KJ Jackson <kjjackson619@yahoo.com>
2022-04-12 21:23:41 -0400

    mutations and queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,41 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {
+            _id
+        }
+    }
+}
+`;
+
+
+export const ADD_ORDER = gql`
+mutation addOrder($products: [ID!) {
+    addOrder(products: $products) {
+        purchaseDate
+        shirts {
+            _id
+            name
+            color
+            price
+            category {
+                name
+            }
+        }
+    }
+}
+`;
+
+export const ADD_USER = gql`
+mutation addUser ($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+        token
+        user {
+            _id
+        }
+    }
+}
+`;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,53 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_SHIRTS = gql`
+query getProducts($category: ID) {
+    products(category: $category) {
+        _id
+        name
+        color
+        price
+        category {
+            _id
+        }
+    }
+}
+`;
+
+export const QUERY_CHECKOUT = gql`
+query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+        session
+    }
+}
+`;
+
+export const QUERY_CATEGORIES = gql`
+{
+    categories {
+        _id
+        name
+    }
+}
+`;
+
+export const QUERY_USER = gql`
+{
+    user {
+        firstName
+        lastName
+        orders {
+            _id
+            purchaseDate
+            shirts {
+                _id
+                name
+                color
+                price
+                category
+                image
+            }
+        }
+    }
+}
+`;
