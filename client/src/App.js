4f72f510e70829125b2ba4e5ba3f868f636d49cb
KJ Jackson <kjjackson619@yahoo.com>
2022-04-12 22:37:34 -0400

    utils

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { setContext } from '@apollo/client/link/context';
 
 import Nav from './components/Nav';
+import Home from './pages/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Success from './pages/Success';
+import OrderHistory from './pages/OrderHistory';
+import Detail from './pages/Detail';
 import SideNav from './components/SideNav';
 import Footer from './components/Footer';
 import NoMatch from './components/NoMatch';
@@ -37,8 +41,12 @@ function App() {
         <div>
           <Nav />
           <Switch>
+            <Route exact path='/' component={Home} />
             <Route exact path='/login' component={Login} />
             <Route exact path='/signup' component={Signup} />
+            <Route exact path='/success' component={Success} />
+            <Route exact path='/orderHistory' component={OrderHistory} />
+            <Route exact path='/shirts/:id' component={Detail} />
             <Route component={NoMatch} />
 
           </Switch>
