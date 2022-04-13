95a738c89328216e533ffa433c2effe5451eb765
KJ Jackson <kjjackson619@yahoo.com>
2022-04-13 08:31:24 -0400

    seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -28,7 +28,7 @@ db.once('open', async () => {
             name: 'Black Short Sleeve T-Shirt',
             color: 'black',
             price: 9.99,
-            category: 'T-Shirt (Short Sleeve)',
+            category: categories[0]._id,
             image: 'shortsleeve-black-1.jpg',
 
         },
@@ -36,70 +36,70 @@ db.once('open', async () => {
             name: 'Black Long Sleeve T-Shirt',
             color: 'Black',
             price: 14.99,
-            category: 'T-Shirt (Long Sleeve)',
+            category: categories[1]._id,
             image: 'longsleeve-black-1.jpg',
         },
         {
             name: 'Navy Hoodie',
             color: 'Navy',
             price: 34.99,
-            category: 'Hoodie',
+            category: categories[2]._id,
             image: 'hoodie-navy-1.jpg',
         },
         {
             name: 'Blue Crewneck',
             color: 'Blue',
             price: 29.99,
-            category: 'Crewneck',
+            category: categories[3]._id,
             image: 'crewneck-blue-1.jpg',
         },
         {
             name: 'Black Tank Top',
             color: 'Black',
             price: 8.99,
-            category: 'Tank Top',
+            category: categories[4]._id,
             image: 'tanktop-black-1.jpg',
         },
         {
             name: 'Light Blue Dress Shirt',
             color: 'Light Blue',
             price: 39.99,
-            category: 'Dress Shirt',
+            category: categories[5]._id,
             image: 'dress-lightblue-1.jpg',
         },
         {
             name: 'Blue Sweater',
             color: 'Blue',
             price: 29.99,
-            category: 'Sweater',
+            category: categories[6]._id,
             image: 'sweater-blue-1.jpg',
         },
         {
             name: 'Black Coat',
             color: 'Black',
             price: 99.99,
-            category: 'Coat/Jacket',
+            category: categories[7]._id,
             image: 'coat-black-1.jpg',
         },
         {
             name: 'White Flannel with Black Stripes',
             color: 'White/Black',
             price: 59.99,
-            category: 'Flannel',
+            category: categories[8]._id,
             image: 'flannel-whiteblack-1.jpg',
         },
         {
             name: 'Pink Polo',
             color: 'Pink',
             price: 39.99,
-            category: 'Polo',
+            category: categories[9]._id,
             image: 'polo-pink-1.jpg',
         },
         {
             name: 'Black Womens Cut T-Shirt',
             color: 'Black',
             price: 9.99,
-            category: 'Womens',
+            category: categories[10]._id,
             image: 'womens-black-1.jpg ',
         }
     ]);
@@ -112,7 +112,7 @@ db.once('open', async () => {
         firstName: 'Kevin',
         lastName: 'Jackson',
         email: 'kjackson@test.com',
-        password: 'passwordkjackson',
+        password: 'Password1!kjackson',
         orders: [
             {
                 shirts: [shirts[0]._id]
@@ -124,7 +124,7 @@ db.once('open', async () => {
         firstName: 'Jonathan',
         lastName: 'Chastain',
         email: 'jchastain@test.com',
-        password: 'passwordjchastain',
+        password: 'Password1!jchastain',
         orders: [
             {
                 shirts: [shirts[0].id, shirts[4].id, shirts[6].id]
@@ -136,7 +136,7 @@ db.once('open', async () => {
         firstName: 'Nikhil',
         lastName: 'Makhija',
         email: 'nmakhija@test.com',
-        password: 'passwordnmakhija',
+        password: 'Password1!nmakhija',
         orders: [
             {
                 shirts: [shirts[9].id, shirts[10].id]
@@ -148,7 +148,7 @@ db.once('open', async () => {
         firstName: 'Matthew',
         lastName: 'Brining',
         email: 'mbrining@test.com',
-        password: 'passwordmbrining'
+        password: 'Password1!mbrining'
     });
 
     console.log('Users Seeded');
